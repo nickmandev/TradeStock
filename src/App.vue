647f89281716bb43c676a26d2c2eee11b5e14faa
nickmandev <nickmandev@gmail.com>
2017-12-10 14:02:48 +0200

    Implemented Vuex, added Stocks store managment

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
   <div class="container">
+    <app-header></app-header>
+    <div class="row">
+      <div class="col-xs-12">
+        <router-view></router-view>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
 export default {
-  data () {
+  components: {
+    appHeader: Header
+  },
+  created() {
+    this.$store.dispatch('initStocks')
   }
 }
 </script>
 
 <style lang="scss">
-
+  $body-padding: 30px;
+  body {
+    padding: $body-padding;
+    box-sizing: border-box;
+  }
 </style>
